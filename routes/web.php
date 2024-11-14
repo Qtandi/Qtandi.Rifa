c8382110e670bfa64db8e8fc559f12291c32f49d
mdolli <iagologo1@gmail.com>
2024-11-14 08:22:09 -0400

    Primeira commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CheckoutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/checkout', [CheckoutController::class, 'realizarCheckout']);
